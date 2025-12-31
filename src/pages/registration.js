c01328157a48c390ff9ad1cd9e8acf7fa8b51e52
Sudesh Rani <130635615+Jsudesh@users.noreply.github.com>
2025-12-31 07:01:41 +0000

    registration changes

--- src/pages/registration.js
+++ src/pages/registration.js
@@ -267,9 +267,8 @@ function Registration() {
                             The registration fee include the conference kit,
                             access/admission to tutorials &amp; technical
                             sessions, lunch on all conference days, morning and
-                            evening refreshments, banquet dinner,
-                            access/admission to cultural events as part of the
-                            conference.
+                            evening refreshments, access/admission to 
+                            cultural events as part of the conference.
                         </li>
                         <li>
                             Please note that the fee shown above are exclusive
