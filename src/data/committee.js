791f2d3cb9000d076fc6942972e683403c56e202
Sudesh Rani <130635615+Jsudesh@users.noreply.github.com>
2026-06-13 15:46:05 +0000

    update

--- src/data/committee.js
+++ src/data/committee.js
@@ -941,7 +941,7 @@ export const COMMITTEE = {
      "Professional Conference Organiser (Official Event Partner)": [
         {
             name: "FORCE (Future Oriented Research Conferences and Exhibitions) Pvt. Ltd.",
-            designation: "Contact Person: Mr. Deepak Aggarwal (Contact: +91-9899969931)",
+            // designation: "Contact Person: Mr. Deepak Aggarwal (Contact: +91-9899969931)",
             image_url: "/assets/committee/force-main-logo.png",
         },
     ]
