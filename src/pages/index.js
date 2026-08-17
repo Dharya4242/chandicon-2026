45c9537a0ef854d591dd1dcc50821104ac05a7e9
Sudesh Rani <130635615+Jsudesh@users.noreply.github.com>
2026-08-17 23:08:21 +0530

    Update index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -103,7 +103,7 @@ export default function Home() {
                         📢{" "}
 
                         <a
-                            href="/Final Schedule_Chandicon 2026 v2.pdf"
+                            href="/Final Schedule_Chandicon 2026 v3.pdf"
                             target="_blank"
                             rel="noopener noreferrer"
                             className={styles.noticeLink}
